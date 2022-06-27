1cbf61375bdeeccf81e7c5d551631c6811cd435c
Alex Siu <105384095+alexandersiu@users.noreply.github.com>
2022-06-27 14:04:07 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from league_stats import get_rank
 from valorant_stats import val_stats
 from fancam import get_Fancam
+from webtoon import Webtoon
+from league import League
 
 load_dotenv()
 
@@ -14,9 +16,8 @@ client = discord.Client() #gets connect to Discord
 
 @client.event #is used to register an event
 async def on_ready(): #is called when the bot is ready to start being used
-    print('We have logged in as {0.user}'.format(client)) 
+    print('Connected') 
 
-nickname_dict = {}
 @client.event
 async def on_message(message): #event triggers each time a message is received
     if message.author == client.user:
@@ -49,21 +50,35 @@ async def on_message(message): #event triggers each time a message is received
             await message.channel.send(embed=embed)
     if message.content.startswith('$love'):
         string = message.content[6:]
-        target1 = string.split(', ')[0]
-        target2 = string.split(', ')[1]
-        loveAmount = random.randint(0, 100)
-        embed = discord.Embed(title = 'Lover Meter', color = 0x00ff00)
-        embed.add_field(name = target1 + ' loves ' + target2, value = str(loveAmount) + '%', inline = False)
-        embed.set_thumbnail(url='https://thumbs.dreamstime.com/z/love-meter-heart-indicator-day-full-test-valentine-background-card-progress-171670379.jpg')
-        await message.channel.send(embed=embed)
+        try:
+            target1 = string.split(', ')[0]
+            target2 = string.split(', ')[1]
+            loveAmount = random.randint(0, 100)
+            embed = discord.Embed(title = 'Lover Meter', color = 0x00ff00)
+            embed.add_field(name = target1 + ' loves ' + target2, value = str(loveAmount) + '%', inline = False)
+            embed.set_thumbnail(url='https://thumbs.dreamstime.com/z/love-meter-heart-indicator-day-full-test-valentine-background-card-progress-171670379.jpg')
+            await message.channel.send(embed=embed)
+        except:
+            await message.channel.send('Add a comma between the two objects')
     if message.content.startswith('$fancam'):
         idol = message.content[8:]
         video_Info = get_Fancam(idol)
-        URLs = list(video_Info.keys())
-        view_Count = list(video_Info.values())
-        embed = discord.Embed(title = idol + '\'s Top 5 Fancams on YouTube', color = 0x00ff00)
-        for i in range(5):
-            embed.add_field(name=URLs[i], value= str(view_Count[i]) + ' views', inline = False)
+        if video_Info == False:
+            await message.channel.send('Invalid input')
+        else:
+            URLs = list(video_Info.keys())
+            view_Count = list(video_Info.values())
+            embed = discord.Embed(title = idol + '\'s Top 5 Fancams on YouTube', color = 0x00ff00)
+            for i in range(5):
+                embed.add_field(name=URLs[i], value= str(view_Count[i]) + ' views', inline = False)
+            await message.channel.send(embed=embed)
+    if message.content.startswith('$webtoon'):
+        webtoon_info = Webtoon()
+        embed = discord.Embed(title=webtoon_info.webtoon_title(), url=webtoon_info.webtoon_url())
+        embed.set_thumbnail(url=webtoon_info.webtoon_img())
+        embed.add_field(name='Rating:', value=webtoon_info.webtoon_rating() + ' / 5')
+        embed.add_field(name='Summary:', value=webtoon_info.webtoon_summary())
         await message.channel.send(embed=embed)
+
 client.run(TOKEN)
 
